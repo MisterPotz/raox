2fca2dde766ea2b18ef72182033c14a08540f3c6
algor <aleksgornostaev@gmail.com>
2021-05-29 12:55:33 +0300

    get initialization scope class and field via preinitialization info built-in methods and reference CodeGenerationContract from lib package

---
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/simulator/SimulatorUtils.java
@@ -0,0 +1,5 @@
+package ru.bmstu.rk9.rao.lib.simulator;
+
+public class SimulatorUtils{
+    public static 
+}
--- ru.bmstu.rk9.rao/src/ru/bmstu/rk9/rao/jvmmodel/GeneratedCodeContract.java
+++ ru.bmstu.rk9.rao/src/ru/bmstu/rk9/rao/jvmmodel/GeneratedCodeContract.java
@@ -1,8 +1,10 @@
 package ru.bmstu.rk9.rao.jvmmodel;
 
+import ru.bmstu.rk9.rao.lib.contract.RaoGenerationContract;
+
 public class GeneratedCodeContract {
-    public static final String INITIALIZATION_SCOPE_CLASS = "InitializationScope";
-    public static final String INITIALIZATION_SCOPE_FIELD = "initializationScope";
+    public static final String INITIALIZATION_SCOPE_CLASS = RaoGenerationContract.INITIALIZATION_SCOPE_CLASS;
+    public static final String INITIALIZATION_SCOPE_FIELD = RaoGenerationContract.INITIALIZATION_SCOPE_FIELD;
 
     private GeneratedCodeContract() {}
 }
