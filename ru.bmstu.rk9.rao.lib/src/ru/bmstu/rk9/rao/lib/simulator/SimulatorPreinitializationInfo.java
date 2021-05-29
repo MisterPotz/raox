273279660abebfd74aea3eaae688d014ae4316b1
algor <aleksgornostaev@gmail.com>
2021-05-29 12:55:33 +0300

    get initialization scope class and field via preinitialization info built-in methods and reference CodeGenerationContract from lib package

--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/simulator/SimulatorPreinitializationInfo.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/simulator/SimulatorPreinitializationInfo.java
@@ -1,13 +1,13 @@
 package ru.bmstu.rk9.rao.lib.simulator;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Stream;
-
-import com.google.common.collect.Streams;
-
+import ru.bmstu.rk9.rao.lib.contract.RaoGenerationContract;
 import ru.bmstu.rk9.rao.lib.json.JSONArray;
 import ru.bmstu.rk9.rao.lib.json.JSONObject;
 import ru.bmstu.rk9.rao.lib.modeldata.ModelStructureConstants;
@@ -25,8 +25,9 @@ public class SimulatorPreinitializationInfo {
 	public final JSONObject modelStructure;
 	public final List<Class<?>> resourceClasses = new ArrayList<>();
 	
-	public Class<?> modelClass;
-	public Class<?> initializationScopeClass;
+	private Class<?> modelClass;
+	private Class<?> initializationScopeClass;
+	private Field initializationScopeField;
 	
 //	public final List<Runnable> resourcePreinitializers = new ArrayList<>();
 	public final List<Function<Object, Runnable>> resourcePreinitializerCreators = new ArrayList<>();
@@ -41,5 +42,43 @@ public class SimulatorPreinitializationInfo {
 				.put(ModelStructureConstants.SEARCHES, new JSONArray());
 	}
 	
+	public void setModelClass(Class<?> modelClass) {
+		this.modelClass = modelClass;
+		this.initializationScopeClass = findInitializationScopeClass(modelClass);
+		this.initializationScopeField = findInitializationScopeField(modelClass);
+	}
 
+	private static Class<?> findInitializationScopeClass(Class<?> modelClass) {
+		Optional<Class<?>> optionalClass = Arrays.asList(modelClass.getDeclaredClasses())
+		.stream().filter(clazz -> clazz.getName().equals(RaoGenerationContract.INITIALIZATION_SCOPE_CLASS)).findFirst();
+
+		if (optionalClass.isPresent()) {
+			return optionalClass.get();
+		}
+		return null;
+	}
+
+	private static Field findInitializationScopeField(Class<?> modelClass) {
+		Optional<Field> optionalField = Arrays.asList(modelClass.getDeclaredFields())
+		.stream().filter(field -> field.getName().equals(RaoGenerationContract.INITIALIZATION_SCOPE_FIELD))
+		.findFirst();
+		
+		if (optionalField.isPresent()) {
+			return optionalField.get();
+		} else {
+			return null;
+		}
+	}
+
+	public Class<?> getModelClass() {
+		return modelClass;
+	}
+
+	public Class<?> getInitializationScopeClass() {
+		return initializationScopeClass;
+	}
+
+	public Field getInitializationScopeField() {
+		return initializationScopeField;
+	}
 }
