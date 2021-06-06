4c086656c20e1af4843fee0400d97b2bcb6b6775
algor <aleksgornostaev@gmail.com>
2021-06-06 22:01:20 +0300

    modify notifier so subscribers can accept payload

--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/notification/Notifier.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/notification/Notifier.java
@@ -6,54 +6,77 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import ru.bmstu.rk9.rao.lib.notification.Subscription.SubscriptionType;
 
-public class Notifier<Category extends Enum<Category>> {
+public class Notifier<T extends Enum<T>> {
 
 	private class SubscriptionState {
 		private final Subscription subscription = new Subscription();
 		private boolean hadNotifications = false;
+		private Object latestPayload = null;
+
+		private void updateState(Boolean hadNotifications, Object latestPayload) {
+			this.hadNotifications = hadNotifications;
+			this.latestPayload = latestPayload;
+		}
 	}
 
-	private final Map<Category, SubscriptionState> subscriptionStates;
+	private final Map<T, SubscriptionState> subscriptionStates;
 
-	public Notifier(Class<Category> enumClass) {
-		subscriptionStates = new ConcurrentHashMap<Category, SubscriptionState>();
+	public Notifier(Class<T> enumClass) {
+		subscriptionStates = new ConcurrentHashMap<T, SubscriptionState>();
 
-		for (Category category : enumClass.getEnumConstants()) {
+		for (T category : enumClass.getEnumConstants()) {
 			subscriptionStates.put(category, new SubscriptionState());
 		}
 	}
 
-	public void notifySubscribers(Category category) {
+	public void notifySubscribers(T category) {
+		notifySubscribers(category, null);
+	}
+
+	public void notifySubscribers(T category, Object payload) {
 		SubscriptionState subscriptionState = subscriptionStates.get(category);
 		Subscription subscription = subscriptionState.subscription;
 
 		for (Subscriber subscriber : subscription.subscribers.keySet()) {
 			if (subscription.subscribers.get(subscriber).contains(SubscriptionType.ONE_SHOT))
 				subscription.removeSubscriber(subscriber);
-			subscriber.fireChange();
+			
+			notifySubscriber(subscriber, payload);
 		}
 
 		subscriptionState.hadNotifications = true;
+		subscriptionState.updateState(true, payload);
 	}
 
-	public void addSubscriber(Subscriber subscriber, Category category) {
+	/**
+	 * don't fire event with payload if subscriber doesn't accept payload or if payload is null
+	 */
+	private void notifySubscriber(Subscriber subscriber, Object payload) {
+		if (subscriber.acceptsPayload() && payload != null) {
+			subscriber.fireChangeWithPayload(payload);
+		} else if (!subscriber.acceptsPayload()) {
+			subscriber.fireChange();
+		}
+	}
+
+	public void addSubscriber(Subscriber subscriber, T category) {
 		addSubscriber(subscriber, category, EnumSet.noneOf(SubscriptionType.class));
 	}
 
-	public void addSubscriber(Subscriber subscriber, Category category, EnumSet<SubscriptionType> subscriptionFlags) {
+	public void addSubscriber(Subscriber subscriber, T category, EnumSet<SubscriptionType> subscriptionFlags) {
 		SubscriptionState subscriptionState = subscriptionStates.get(category);
 
 		subscriptionState.subscription.addSubscriber(subscriber, subscriptionFlags);
 
 		if (!subscriptionFlags.contains(SubscriptionType.IGNORE_ACCUMULATED) && subscriptionState.hadNotifications)
-			subscriber.fireChange();
+			notifySubscriber(subscriber, subscriptionState.latestPayload);
 	}
 
-	public void removeSubscriber(Subscriber subscriber, Category category) {
+	public void removeSubscriber(Subscriber subscriber, T category) {
 		subscriptionStates.get(category).subscription.removeSubscriber(subscriber);
 	}
 
-	public void removeAllSubscribers(Category category) {
+	public void removeAllSubscribers(T category) {
 		subscriptionStates.get(category).subscription.subscribers.clear();
 	}
 }
