1ea78cc7c0e5ceb243f146f222e159e6118cd755
algor <aleksgornostaev@gmail.com>
2021-06-07 04:17:41 +0300

    add builder methods to result class

--- ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/result/Result.java
+++ ru.bmstu.rk9.rao.lib/src/ru/bmstu/rk9/rao/lib/result/Result.java
@@ -3,6 +3,38 @@ package ru.bmstu.rk9.rao.lib.result;
 import ru.bmstu.rk9.rao.lib.simulatormanager.SimulatorId;
 
 public class Result {
+	private SimulatorId simulatorId;
+
+	public Result(SimulatorId simulatorId) {
+		this.simulatorId = simulatorId;
+	}
+
+	public <T> UpdatableResult<T> create(Statistics<T> statistics) {
+		return new UpdatableResult<T>(statistics, simulatorId);
+	}
+
+	public <T> EvaluatableResult<T> create(AbstractDataSource<T> dataSource, ResultMode resultMode,
+			Statistics<T> statistics) {
+		return new EvaluatableResult<T>(dataSource, resultMode, statistics, simulatorId);
+	}
+
+	public <T> EvaluatableResult<T> create(AbstractDataSource<T> dataSource,
+			Statistics<T> statistics) {
+		return new EvaluatableResult<T>(dataSource, ResultMode.AUTO, statistics, simulatorId);
+	}
+
+	public <T> EvaluatableResult<T> create(AbstractDataSource<T> dataSource,
+			ResultMode resultMode) {
+		return new EvaluatableResult<T>(dataSource, resultMode, dataSource.getDefaultStatistics(),
+				simulatorId);
+	}
+
+	public <T> EvaluatableResult<T> create(AbstractDataSource<T> dataSource) {
+		return new EvaluatableResult<T>(dataSource, ResultMode.AUTO,
+				dataSource.getDefaultStatistics(), simulatorId);
+	}
+
+	// Utility methods
 
 	static public <T> UpdatableResult<T> create(
 			Statistics<T> statistics, 
